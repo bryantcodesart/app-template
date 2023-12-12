414d9bddedc4d74d244e849f2c887b528ed25d03
bryantcodesart <bryant@bryantcodes.art>
2023-12-12 01:26:43 -0600

    Import queries into nextjs

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,3 +1,16 @@
+import { getBlogListing } from '@mono/queries';
+
 export default async function Index() {
-  return <div className="p-4 text-red-500">Super cool site!</div>;
+  const blogArticles = await getBlogListing();
+
+  return (
+    <ul>
+      {blogArticles.map((article) => (
+        <li key={article.title}>
+          <h2>{article.title}</h2>
+          <p>{article.excerpt}</p>
+        </li>
+      ))}
+    </ul>
+  );
 }
